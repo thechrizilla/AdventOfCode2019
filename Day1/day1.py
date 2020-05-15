002f7fb53e26ec9a3b1df2625e22fc0b232d5b8b
thechrizilla <chrisck.chan@hotmail.com>
2020-05-15 09:46:53 -0400

    Added complete solution for day 1

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,15 +1,32 @@
 import math
 
-def fuel_mass(mass):
+def fuel(mass):
     fuel = math.floor(mass/3) - 2
     return fuel
 
-def total_mass():
+def total_fuel():
     file = open("day1_input.txt", "r")
     sum = 0
     inputs = file.readlines()
     for input in inputs:
-        sum += fuel_mass(int(input))
+        sum += fuel(int(input))
     return int(sum)
 
-print(total_mass())
+def fuel_c(mass):
+    t_fuel = fuel(mass)
+    if t_fuel <= 0:
+        return 0
+    else:
+        return t_fuel + fuel_c(t_fuel)
+
+def total_fuel_c():
+    file = open("day1_input.txt", "r")
+    sum = 0
+    inputs = file.readlines()
+    for input in inputs:
+        sum += fuel_c(int(input))
+    return int(sum)
+
+
+print(total_fuel())
+print(total_fuel_c())
